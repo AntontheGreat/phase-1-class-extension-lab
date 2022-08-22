a5869ff95e9659d6f6b5da99d5ad61166abb4fca
Antonio <antonio18casta@aol.com>
2022-08-22 16:51:51 -0400

    commit tree a134cbf3e44937b8730a3fd3e54f733582a79387

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Your code here
+class Polygon {
+    constructor(polygon) {
+        this.polygon = polygon;
+        let array = [];
+    }
+
+    get countSides() {
+        return this.polygon.length;
+    }
+
+    get perimeter() {
+        this.perimeter = array.reduce((accumlator, value) => {
+            return accumlator + value;
+        }, 0);
+        return this.perimeter
+    }
+};
+
+class Triangle extends Polygon {
+    get isValid() {
+
+    }
+};
+
+class Square extends Polygon {
+
+};
